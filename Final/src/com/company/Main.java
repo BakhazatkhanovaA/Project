ba908ef315b1e039432d5956881e50e8e82beac1
BakhazatkhanovaA <57889719+BakhazatkhanovaA@users.noreply.github.com>
2020-06-12 13:01:41 +0600

    Update Main.java

--- Final/src/com/company/Main.java
+++ Final/src/com/company/Main.java
@@ -2,7 +2,15 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+
+        public static void main(String[] args) {
+            Customer cus=new Customer("Aruzhan", "Bakhazatkhanova", "12345");
+            // System.out.println(cus.getLogin());
+            Account account=new Account(500,500,100, "True");
+            //cus.CustomerInformation();
+            Database rest=Database.getInstance();
+            rest.getdata("SELECT * FROM my.customer");
+        }
     }
-}
+
+
