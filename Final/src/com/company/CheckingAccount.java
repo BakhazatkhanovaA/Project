e8f2a2ab959e6f25b7fe420f89a4c97ebaa8ade9
BakhazatkhanovaA <57889719+BakhazatkhanovaA@users.noreply.github.com>
2020-06-12 12:57:10 +0600

    Update CheckingAccount.java

--- Final/src/com/company/CheckingAccount.java
+++ Final/src/com/company/CheckingAccount.java
@@ -1,23 +1,33 @@
 package com.company;
 
-public class CheckingAccount extends Account  implements Control{
+public class CheckingAccount extends Account {
 
-    public CheckingAccount(int variable, double deposit, double withdraw, boolean balanceEnquiry) {
+    public CheckingAccount(int variable, double deposit, double withdraw, String balanceEnquiry) {
         super(variable, deposit, withdraw, balanceEnquiry);
     }
 
     double overDraft = -500;
-
+//Checking is it possible or not to gain credit
     public void creditLimit(double i) {
+        if (i <= 0 && i >= overDraft) {
+            withdraw(50);
+            System.out.println("The account is overdrawn.");
+        } else if (i <= 0 && i <= overDraft) {
+            withdraw(20);
+            System.out.println("The account is locked until a deposit is made to bring the account up to a positive value.");
+        }
+    }
+    @Override
+    public String toString(){
+        System.out.println("If this account satisfied to "+creditLimit()+" you can get overdraft!");
 
-        //initializes double variable balance as 0.0
-        double balance = 0.0;
-        if (balance - i < overDraft){
-            System.out.println("Failure: Can't overdraft more than $500.");
-            balance = balance - 25; }
-        else
-            balance = balance - i;
+        return null;
     }
 
-
+    private String creditLimit() {
+        return "The account is overdrawn";
+    }
 }
+
+
+
